c20ffa3d44c2124165770d9cf3cc89970c38f685
kyang5 <kyang5@luc.edu>
2022-03-22 23:14:40 -0500

    Initial commit

--- src/com/university/model/use/Type.java
+++ src/com/university/model/use/Type.java
@@ -2,9 +2,14 @@ package com.university.model.use;
 
 import com.university.model.facility.FacilityRoom;
 
+import java.util.Date;
+
 public class Type {
     private String facilityUseType;
     private FacilityRoom facilityRoom;
+    private Date useStartDate;
+    private Date useEndDate;
+    public int occupancy;
 
     public String getFacilityUseType() {
         return facilityUseType;
@@ -14,6 +19,7 @@ public class Type {
         this.facilityUseType = facilityUseType;
     }
 
+    // assign facility room to use type
     public FacilityRoom getFacilityRoom() {
         return facilityRoom;
     }
@@ -21,4 +27,29 @@ public class Type {
     public void setFacilityRoom(FacilityRoom facilityRoom) {
         this.facilityRoom = facilityRoom;
     }
+
+    public Date getUseStartDate() {
+        return useStartDate;
+    }
+
+    public void setUseStartDate(Date useStartDate) {
+        this.useStartDate = useStartDate;
+    }
+
+    public Date getUseEndDate() {
+        return useEndDate;
+    }
+
+    public void setUseEndDate(Date useEndDate) {
+        this.useEndDate = useEndDate;
+    }
+
+    public int getOccupancy() {
+        return occupancy;
+    }
+
+    public void setOccupancy(int occupancy) {
+        this.occupancy = occupancy;
+    }
+
 }
