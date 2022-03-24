fa7012929893bc15c7d5514c0d39223f4b10da98
kyang5 <kyang5@luc.edu>
2022-03-24 18:35:55 -0500

    Amended

--- src/com/university/model/facility/IFacilityManager.java
+++ src/com/university/model/facility/IFacilityManager.java
@@ -9,7 +9,7 @@ public interface IFacilityManager {
     public void setManagerLastName(String managerLastName);
     public int getManagerId();
     public void setManagerId(int managerId);
-    // same as listFacilities()
+    // returns a list of all facilities a facility manager is assigned to
     public List<IFacilityLocation> getListFacilities();
     public void setListFacilities(List<IFacilityLocation> listFacilities);
     public void addFacilities(IFacilityLocation facilityLocation);
