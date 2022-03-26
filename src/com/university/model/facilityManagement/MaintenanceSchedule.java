82729be8905ce09f394468d66d58674a11211af9
kyang5 <kyang5@luc.edu>
2022-03-26 17:32:19 -0500

    Amended

--- src/com/university/model/facilityManagement/MaintenanceSchedule.java
+++ src/com/university/model/facilityManagement/MaintenanceSchedule.java
@@ -1,11 +1,9 @@
 package com.university.model.facilityManagement;
-import com.university.model.facility.FacilityRoom;
 import com.university.model.facility.IFacilityRoom;
 
 import java.util.*;
 
 public class MaintenanceSchedule implements ISchedule {
-    //private Date maintenanceDate;
     private Date maintenanceStartDate;
     private Date maintenanceEndDate;
     private int scheduleID;
@@ -49,8 +47,8 @@ public class MaintenanceSchedule implements ISchedule {
     public long calcDownTimeForFacilityRoom(Date maintenanceStartDate, Date maintenanceEndDate){
         long startDate = maintenanceStartDate.getTime();
         long endDate = maintenanceEndDate.getTime();
-        long downTime = endDate - startDate;
-        return downTime;
+
+        return endDate - startDate;
     }
 }
 
