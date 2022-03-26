88c014a7fd9b7cf5ab3847ca983c8a47944f5e77
kyang5 <kyang5@luc.edu>
2022-03-26 17:21:38 -0500

    Amended

--- src/com/university/model/facilityManagement/IWorker.java
+++ src/com/university/model/facilityManagement/IWorker.java
@@ -1,12 +1,12 @@
 package com.university.model.facilityManagement;
 
 public interface IWorker {
-    public String getMaintFirstName();
-    public String getMaintLastName();
-    public int getMaintWorkerID();
-    public String getMaintTitle();
-    public void setMaintFirstName(String firstName);
-    public void setMaintLastName(String lastName);
-    public void setMaintWorkerID(int maintenanceWorkerID);
-    public void setMaintTitle(String title);
+    String getMaintFirstName();
+    String getMaintLastName();
+    int getMaintWorkerID();
+    String getMaintTitle();
+    void setMaintFirstName(String firstName);
+    void setMaintLastName(String lastName);
+    void setMaintWorkerID(int maintenanceWorkerID);
+    void setMaintTitle(String title);
 }
